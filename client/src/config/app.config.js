2838dff35828d1731761dd7b585b6ecbf4920192
Jason Quesada <jasonquesadaarguedas@gmail.com>
2026-08-20 10:06:28 -0600

    feat: add application configuration

---
+++ client/src/config/app.config.js
@@ -0,0 +1,7 @@
+const appConfig = {
+  name: 'Portfolio Platform',
+  description:
+    'Reusable full-stack portfolio platform and template built with React, JavaScript, Node.js, and Firebase.',
+}
+
+export default appConfig
