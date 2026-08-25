1143bd72cf1313dc27b859249a8becd58d6a0d6e
Jason Quesada <jasonquesadaarguedas@gmail.com>
2026-08-24 18:38:03 -0600

    feat: protect content write routes with admin authorization

--- server/src/routes/experiences.routes.js
+++ server/src/routes/experiences.routes.js
@@ -8,13 +8,36 @@ import {
   updateExperienceController,
 } from '../controllers/experiences.controller.js';
 
+import authenticateAdmin from '../middleware/admin-auth.middleware.js';
+import authorizeAdmin from '../middleware/authorize-admin.middleware.js';
+
 const router = Router();
 
+const adminMiddleware = [
+  authenticateAdmin,
+  authorizeAdmin,
+];
+
 router.get('/', getPublishedExperiencesController);
+
 router.get('/:id', getExperienceController);
 
-router.post('/', createExperienceController);
-router.put('/:id', updateExperienceController);
-router.delete('/:id', deleteExperienceController);
+router.post(
+  '/',
+  ...adminMiddleware,
+  createExperienceController,
+);
+
+router.put(
+  '/:id',
+  ...adminMiddleware,
+  updateExperienceController,
+);
+
+router.delete(
+  '/:id',
+  ...adminMiddleware,
+  deleteExperienceController,
+);
 
 export default router;
--- server/src/routes/projects.routes.js
+++ server/src/routes/projects.routes.js
@@ -8,13 +8,36 @@ import {
   updateProjectController,
 } from '../controllers/projects.controller.js';
 
+import authenticateAdmin from '../middleware/admin-auth.middleware.js';
+import authorizeAdmin from '../middleware/authorize-admin.middleware.js';
+
 const router = Router();
 
+const adminMiddleware = [
+  authenticateAdmin,
+  authorizeAdmin,
+];
+
 router.get('/', getPublishedProjectsController);
+
 router.get('/:id', getProjectController);
 
-router.post('/', createProjectController);
-router.put('/:id', updateProjectController);
-router.delete('/:id', deleteProjectController);
+router.post(
+  '/',
+  ...adminMiddleware,
+  createProjectController,
+);
+
+router.put(
+  '/:id',
+  ...adminMiddleware,
+  updateProjectController,
+);
+
+router.delete(
+  '/:id',
+  ...adminMiddleware,
+  deleteProjectController,
+);
 
 export default router;
